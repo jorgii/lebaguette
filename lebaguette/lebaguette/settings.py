76b785e8f6272fbceaccb17833c06e4ee3ae38e5
George Goranov <ggoranov@mailjet.com>
2016-02-16 13:04:55 +0200

    log to a file

--- lebaguette/lebaguette/settings.py
+++ lebaguette/lebaguette/settings.py
@@ -135,25 +135,20 @@ os.environ['wsgi.url_scheme'] = 'https'
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
     'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR,"log.txt"),
+        },
     },
     'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
             'propagate': True,
         },
-    }
+    },
 }
 
 try:
