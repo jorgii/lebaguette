2ef64fdcfb52d9cfe36f27ff30ea2c852cb8e35e
George Goranov <ggoranov@mailjet.com>
2016-02-16 12:55:18 +0200

    logging

--- lebaguette/lebaguette/settings.py
+++ lebaguette/lebaguette/settings.py
@@ -138,6 +138,30 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 # wsgi scheme
 os.environ['wsgi.url_scheme'] = 'https'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
+
 try:
     from lebaguette.local_settings import *
 except ImportError as e:
