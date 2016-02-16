a36f7dd533ed3f6911cd04c6b7c63718ea0f8987
George Goranov <ggoranov@mailjet.com>
2016-02-16 12:21:40 +0200

    remove status from master

--- lebaguette/lebaguette/urls.py
+++ lebaguette/lebaguette/urls.py
@@ -20,5 +20,4 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', 'home.views.home', name='home'),
-    url(r'^status/', 'status.views.server_status', name='status'),
 ]
