8299d8d5c8439ed34c704da5968af5ad309494ba
George Goranov <ggoranov@mailjet.com>
2016-02-19 16:10:40 +0200

    add method for cpu

--- lebaguette/status/ajax.py
+++ lebaguette/status/ajax.py
@@ -1,1 +1,20 @@
+import psutil
+from datetime import datetime
+
+
 import json
+from django.http import Http404, HttpResponse
+
+
+
+def get_cpu_usage(request):
+    if request.is_ajax():
+        cpu_count = psutil.cpu_count()
+        cpu_usage = psutil.cpu_percent(percpu=True)
+        data = {}
+        data['cpu_count'] = cpu_count
+        data['cpu_usage'] = cpu_usage
+        data = json.dumps(data)
+        return HttpResponse(data,content_type='application/json')
+    else:
+        raise Http404
