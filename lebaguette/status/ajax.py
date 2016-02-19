854535ffc5f241aeea2eb478fa36e6add2eaeeab
George Goranov <ggoranov@mailjet.com>
2016-02-19 16:17:41 +0200

    add method for memory

--- lebaguette/status/ajax.py
+++ lebaguette/status/ajax.py
@@ -18,3 +18,18 @@ def get_cpu_usage(request):
         return HttpResponse(data,content_type='application/json')
     else:
         raise Http404
+
+
+def get_ram_usage(request):
+    if request.is_ajax():
+        memory = psutil.virtual_memory()
+        data = {}
+        data['units'] = 'MB'
+        data['total'] = round(memory.total/1048576,2)
+        data['used'] = round(memory.used/1048576,2)
+        data['available'] = round(memory.available/1048576,2)
+        data['percent'] = memory.percent
+        data = json.dumps(data)
+        return HttpResponse(data,content_type='application/json')
+    else:
+        raise Http404    
