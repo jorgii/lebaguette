d2560776f40aa494e5f4d5032416f539a93ef389
George Goranov <ggoranov@mailjet.com>
2016-02-22 17:07:30 +0200

    move methods to views that do not need to be ajax called

--- lebaguette/status/urls.py
+++ lebaguette/status/urls.py
@@ -5,9 +5,4 @@ from . import views, ajax
 urlpatterns = [
     url(r'^$', views.server_status, name='status'),
     url(r'^cpu$', ajax.get_cpu_usage, name='cpu'),
-    url(r'^ram$', ajax.get_ram_usage, name='ram'),
-    url(r'^disk_data$', ajax.get_disk_data, name='disk_data'),
-    url(r'^disk_usage$', ajax.get_disk_usage, name='disk_usage'),
-    url(r'^uptime$', ajax.get_uptime, name='uptime'),
-    url(r'^service/(?P<servicename>.+)/$', ajax.get_service, name='service'),
 ]
--- lebaguette/status/views.py
+++ lebaguette/status/views.py
@@ -1,7 +1,75 @@
+import psutil
+from datetime import datetime
+from subprocess import check_output, CalledProcessError
+
+
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 
 @login_required
 def server_status(request):
+    ram_usage = get_ram_usage()
+    disk_data = get_disk_data()
+    disk_usage = get_disk_usage()
+    plex_status = get_service('plexmediaserver')
+    transmission_status = get_service('transmission-daemon')
+    apache2_status = get_service('apache2')
     return render(request, 'status/status.html', locals())
+
+
+def get_ram_usage():
+    memory = psutil.virtual_memory()
+    data = {}
+    data['units'] = 'MB'
+    data['total'] = round(memory.total/1048576, 2)
+    data['used'] = round(memory.used/1048576, 2)
+    data['available'] = round(memory.available/1048576, 2)
+    data['percent'] = memory.percent
+    return data
+
+
+def get_disk_data():
+    disk_partitions = psutil.disk_partitions()
+    data = {}
+    for partition in disk_partitions:
+        data[partition.device] = {'device': partition.device,
+                                  'mount_point': partition.mountpoint,
+                                  'fstype': partition.fstype}
+    return data
+
+
+def get_disk_usage():
+    disk_partitions = psutil.disk_partitions()
+    data = {}
+    for partition in disk_partitions:
+        data[partition.device] = {
+         'units': 'GB',
+         'total': round(psutil.disk_usage(partition.mountpoint).total/1073741824, 2),
+         'used': round(psutil.disk_usage(partition.mountpoint).used/1073741824, 2),
+         'free': round(psutil.disk_usage(partition.mountpoint).free/1073741824, 2),
+         'percent': psutil.disk_usage(partition.mountpoint).percent}
+    return data
+
+
+def get_uptime():
+    uptime = datetime.now() - datetime.fromtimestamp(psutil.boot_time())
+    minutes, seconds = divmod(uptime.seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    data = {}
+    data['days'] = days
+    data['hours'] = hours
+    data['minutes'] = minutes
+    data['seconds'] = seconds
+    return data
+
+
+def get_service(servicename):
+    data = {}
+    try:
+        service_data = check_output(["service", servicename, "status"]).decode("utf-8")
+        data[servicename] = service_data
+    except CalledProcessError:
+        data[servicename] = 'Service not found'
+    return data
