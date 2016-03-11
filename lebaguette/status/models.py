d0aff396dbafcc6772ce1ad2eac2aa6e39ec7db5
George Goranov <ggoranov@mailjet.com>
2016-03-11 14:12:12 +0200

    set creation datetime

--- lebaguette/status/models.py
+++ lebaguette/status/models.py
@@ -4,9 +4,9 @@ from django.db import models
 
 
 class Services(models.Model):
-    service_name = models.CharField('City', max_length=255, blank=False, null=False)
-    date_added = models.DateField(auto_now_add=True)
+    service_name = models.CharField('Service Name', max_length=255, blank=False, null=False)
+    datetime_created = models.DateTimeField(auto_now_add=True)
 
 
-def __str__(self):
-    return self.service_name
+    def __str__(self):
+        return self.service_name
