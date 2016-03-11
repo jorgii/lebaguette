d4958863238e1c9427f7608baf43ad2794144351
George Goranov <ggoranov@mailjet.com>
2016-03-11 12:46:09 +0200

    add services module

--- lebaguette/status/models.py
+++ lebaguette/status/models.py
@@ -2,4 +2,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Services(models.Model):
+    service_name = models.CharField('City', max_length=255, blank=False, null=False)
+    date_added = models.DateField(auto_now_add=True)
+
+
+def __str__(self):
+    return self.service_name
