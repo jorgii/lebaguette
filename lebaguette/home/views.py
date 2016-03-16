9be09cffdf30ce0e8854e8eb3498bf947c6588fc
George Goranov <ggoranov@mailjet.com>
2016-03-16 14:36:54 +0200

    add user edit page

--- lebaguette/home/views.py
+++ lebaguette/home/views.py
@@ -1,8 +1,12 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 from django.shortcuts import render, redirect
 from django.core.context_processors import csrf
 from django.contrib.auth import login
 from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+
+
+from .forms import UserForm
 
 
 def login_user(request):
@@ -28,3 +32,16 @@ def login_user(request):
 def logout_user(request):
     logout(request)
     return redirect('/login')
+
+
+@login_required
+def edit_user(request):
+    user = request.user
+    user_change_form = UserForm(request.POST or None)
+    password_change_form = PasswordChangeForm(request.POST or None)
+    if request.method == 'POST':
+        if user_change_form.is_valid():
+            user_change_form.save()
+            return redirect('/profile/')
+    csrf(request)
+    return render(request, 'profile/profile.html', locals())
