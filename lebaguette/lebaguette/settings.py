98ac7ba74aa92856962bf2324b6b86b32bad74ce
George Goranov <ggoranov@mailjet.com>
2016-03-19 13:34:50 +0200

    take secret key from env var

--- lebaguette/lebaguette/settings.py
+++ lebaguette/lebaguette/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(*)4a0m&7z(l5duy++jn%o(de$hfu%gau@$00klb*)4^rvl=yn'
+SECRET_KEY = os.environ.get('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
