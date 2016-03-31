5fe42a6d5b05f3d387e9360c0638b50b59cad1a2
George Goranov <ggoranov@mailjet.com>
2016-03-31 12:28:36 +0300

    remove unnecessary slash

--- lebaguette/home/urls.py
+++ lebaguette/home/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^/$', views.home_page, name='home'),
+    url(r'^$', views.home_page, name='home'),
     url(r'^login/$', views.login_user, name='login'),
     url(r'^logout/$', views.logout_user, name='logout'),
     url(r'^profile/$', views.edit_user, name='profile'),
