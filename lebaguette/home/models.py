5f84806b5ced457b5b20b19f887f00ee71621631
George Goranov <ggoranov@mailjet.com>
2016-04-04 12:46:15 +0300

    add Server Message model

--- lebaguette/home/models.py
+++ lebaguette/home/models.py
@@ -2,4 +2,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class ServerMessage(models.Model):
+    message = models.TextField('Message',
+                               blank=False,
+                               null=False)
+    datetime_created = models.DateTimeField(auto_now_add=True)
