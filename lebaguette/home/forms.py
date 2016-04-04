f6baf1f14ef29e8a2a0f0c09a5eaeca4d1990177
George Goranov <ggoranov@mailjet.com>
2016-04-04 12:47:08 +0300

    add server message form

--- lebaguette/home/forms.py
+++ lebaguette/home/forms.py
@@ -1,8 +1,16 @@
 from django.forms import ModelForm
 from django.contrib.auth.models import User
 
+from .models import ServerMessage
+
 
 class UserForm(ModelForm):
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email']
+
+
+class ServerMessageForm(ModelForm):
+    class Meta:
+        model = ServerMessage
+        fields = ['message']
