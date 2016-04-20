a3110aee88aaa08bd771ec2adee25d69e9e3e0cb
George Goranov <g.p.goranov@gmail.com>
2016-04-20 19:25:35 +0300

    Add create method

--- lebaguette/requestmedia/models.py
+++ lebaguette/requestmedia/models.py
@@ -30,6 +30,13 @@ class TVShowSeason(models.Model):
     tv_show = models.ForeignKey(TVShow, on_delete=models.CASCADE)
     season_number = models.IntegerField(unique=True)
 
+    def __str__(self):
+        return (self.tv_show.title + ': Season ' + str(self.season_number))
+
+    @classmethod
+    def create(cls, tv_show, season_number):
+        return cls(tv_show=tv_show, season_number=season_number)
+
 
 class TVShowEpisode(models.Model):
     season = models.ForeignKey(TVShowSeason, on_delete=models.CASCADE)
@@ -38,6 +45,12 @@ class TVShowEpisode(models.Model):
                                      blank=False,
                                      null=False)
     episode_number = models.IntegerField(unique=True)
+
+    @classmethod
+    def create(cls, season, episode_title, episode_number):
+        return cls(season=season,
+                   episode_title=episode_title,
+                   episode_number=episode_number)
 
 
 class Movie(MediaItem):
