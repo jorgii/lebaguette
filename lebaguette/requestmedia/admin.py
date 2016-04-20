0fb8c782172abc41332031fa73f65765fa9cd47c
George Goranov <g.p.goranov@gmail.com>
2016-04-20 17:51:52 +0300

    Add files

--- lebaguette/requestmedia/admin.py
+++ lebaguette/requestmedia/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from .models import MediaItem
+from .models import TVShow, TVShowSeason, TVShowEpisode, Movie
 
-admin.site.register(MediaItem)
+admin.site.register(TVShow)
+admin.site.register(TVShowSeason)
+admin.site.register(TVShowEpisode)
+admin.site.register(Movie)
--- lebaguette/requestmedia/models.py
+++ lebaguette/requestmedia/models.py
@@ -2,23 +2,44 @@ from django.db import models
 
 
 class MediaItem(models.Model):
-    MEDIA_TYPES = (
-        ('MV', 'Movie'),
-        ('TV', 'TV Show')
-    )
     title = models.CharField('Title',
                              max_length=255,
                              blank=False,
                              null=False)
-    media_type = models.CharField(max_length=2, choices=MEDIA_TYPES)
     released = models.DateField()
     imdb_rating = models.DecimalField(decimal_places=1, max_digits=2)
     imdb_id = models.CharField('IMDB ID',
                                max_length=255,
                                blank=False,
                                null=False)
-    completed = models.BooleanField(default=False)
+    request_completed = models.BooleanField(default=False)
     datetime_created = models.DateTimeField(auto_now_add=True)
 
+    class Meta():
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+
+class TVShow(MediaItem):
+    show_completed = models.BooleanField(default=False)
+
+
+class TVShowSeason(models.Model):
+    tv_show = models.ForeignKey(TVShow, on_delete=models.CASCADE)
+    season_number = models.IntegerField(unique=True)
+
+
+class TVShowEpisode(models.Model):
+    season = models.ForeignKey(TVShowSeason, on_delete=models.CASCADE)
+    episode_title = models.CharField('Episode Title',
+                                     max_length=255,
+                                     blank=False,
+                                     null=False)
+    episode_number = models.IntegerField(unique=True)
+
+
+class Movie(MediaItem):
     def __str__(self):
         return self.title
