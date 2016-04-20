b4cdfbcf33823003ad617be5161db59cbb7bf777
George Goranov <g.p.goranov@gmail.com>
2016-04-20 18:54:54 +0300

    Loop through db and api

--- lebaguette/requestmedia/management/commands/check_new_episodes.py
+++ lebaguette/requestmedia/management/commands/check_new_episodes.py
@@ -2,10 +2,29 @@ import requests
 
 from django.core.management.base import BaseCommand
 
-from .models import TVShow, TVShowSeason, TVShowEpisode
+from requestmedia.models import TVShow, TVShowSeason, TVShowEpisode
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         active_shows = TVShow.objects.filter(show_completed=False)
-        print(active_shows)
+        self.get_new_episodes(active_shows)
+
+    def get_new_episodes(self, active_shows):
+        # loop shows in db
+        for show in active_shows:
+            season = 1
+            request = self.get_season_episodes(show.imdb_id, season)
+            # loop episodes from omdb api
+            while request.json()['Response'] == 'True':
+                print(request.text)
+
+                season += 1
+                request = self.get_season_request(show.imdb_id, season)
+
+    def get_season_episodes(self, imdb_id, season):
+        return requests.get('http://www.omdbapi.com/?i=' +
+                            imdb_id +
+                            '&Season=' +
+                            str(season) +
+                            '&plot=short&r=json')
