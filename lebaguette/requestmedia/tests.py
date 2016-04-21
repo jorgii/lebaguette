de58056b1b96730e28eaaee4d53947ded2bc6b3b
George Goranov <g.p.goranov@gmail.com>
2016-04-21 19:21:50 +0300

    add test to get data from scratch

--- lebaguette/requestmedia/tests.py
+++ lebaguette/requestmedia/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.core.management import call_command
 
-# Create your tests here.
+from requestmedia.models import TVShow, TVShowSeason, TVShowEpisode
+
+
+class CommandsTest(TestCase):
+    def test_get_shows_data(self):
+        call_command(
+            'loaddata', 'tv_shows',
+            verbosity=0)
+        self.assertEqual(len(TVShow.objects.all()), 3)
+        call_command('check_new_episodes')
+        self.assertTrue(TVShowEpisode.objects.all().exists())
