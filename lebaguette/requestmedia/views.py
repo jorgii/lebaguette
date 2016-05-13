e2ad1cba2f41a8d4698a858791464fc47272942a
George Goranov <g.p.goranov@gmail.com>
2016-05-13 15:56:59 +0300

    filter by season completed

--- lebaguette/requestmedia/views.py
+++ lebaguette/requestmedia/views.py
@@ -26,6 +26,7 @@ def request_media(request):
 def episodes(request):
     episodes = TVShowEpisode.objects.filter(
         episode_completed=False,
-        season__tv_show__show_completed=False).order_by(
+        season__tv_show__show_completed=False,
+        season__season_completed=False).order_by(
         Coalesce('episode_released', 'episode_title').desc())
     return render(request, 'requestmedia/episodes.html', locals())
