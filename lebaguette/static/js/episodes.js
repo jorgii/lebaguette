b61432f742e894acef3ab8c63f91e86d8f85a4c1
Martin Martev <mmartev@gmail.com>
2016-06-15 12:31:02 +0300

    update ajax to send csrf token

--- lebaguette/static/js/episodes.js
+++ lebaguette/static/js/episodes.js
@@ -1,26 +1,60 @@
+//GET csrf token
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+var csrftoken = getCookie('csrftoken');
+
+function csrfSafeMethod(method) {
+  // these HTTP methods do not require CSRF protection
+  return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
+
+//Event handler on episode removal
 $(".episode__check").click(function(){
-  var imdbid = $(this).parents("li").attr("id");
+  var imdbid = $(this).parents("li").attr("id"),
+      dataMerged = {"imdbid":imdbid};
   $(this).addClass("episode__checked");
+
   $.ajax({
     type: 'POST',
     url: "/requestmedia/episodes/complete",
-    data: imdbid,
+    data: dataMerged,
     dataType: "text",
     success: function() {
-      $(this).parents("li").fadeOut(800, function(){ $(this).remove();});
-      var snackbarContainer = document.querySelector('#snackbar-success');
-      var data = {
+      $("#"+imdbid).fadeOut(800, function(){ $(this).remove();});
+      var snackbarContainer = document.querySelector('#snackbar-success'),
+          data = {
         message: 'Success!',
         timeout: 3000,
       };
       snackbarContainer.MaterialSnackbar.showSnackbar(data);
     },
-    error: function() {
-      var snackbarContainer = document.querySelector('#snackbar-error');
-      var data = {
+    error: function(ts) {
+      var snackbarContainer = document.querySelector('#snackbar-error'),
+          data = {
         message: 'Could not remove entry '+imdbid,
         timeout: 3000,
       };
+      console.log(ts.responseText);
       snackbarContainer.MaterialSnackbar.showSnackbar(data);
     }
   });
