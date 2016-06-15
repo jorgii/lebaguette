082c51edc38ca8e6b62dd19aa4be0cb85974fabe
George Goranov <g.p.goranov@gmail.com>
2016-06-15 18:29:17 +0300

    get request items

--- lebaguette/requestmedia/views.py
+++ lebaguette/requestmedia/views.py
@@ -4,22 +4,22 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models.functions import Coalesce
 
 
-from .models import TVShowEpisode
+from .models import Request
 from lebaguette.extra import is_in_group
 
 
 @login_required
 def request_media(request):
-    media_items = TVShowEpisode.objects.all().order_by(
-        Coalesce('episode_released', 'episode_title').desc())
-    paginator = Paginator(media_items, 5)
-    page = request.GET.get('page')
-    try:
-        media_items_page = paginator.page(page)
-    except PageNotAnInteger:
-        media_items_page = paginator.page(number=1)
-    except EmptyPage:
-        media_items_page = paginator.page(paginator.num_pages)
+    request_items = Request.objects.filter(status='N').order_by(
+        Coalesce('datetime_requested').desc())
+#    paginator = Paginator(media_items, 5)
+#    page = request.GET.get('page')
+#    try:
+#        media_items_page = paginator.page(page)
+#    except PageNotAnInteger:
+#        media_items_page = paginator.page(number=1)
+#    except EmptyPage:
+#        media_items_page = paginator.page(paginator.num_pages)
     return render(request, 'requestmedia/request_media.html', locals())
 
 
