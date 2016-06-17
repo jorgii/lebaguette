ad6ada62b238cfbd1978d58c2c53574e1892280e
Martin Martev <mmartev@gmail.com>
2016-06-17 13:07:14 +0300

    enable pagination

--- lebaguette/requestmedia/views.py
+++ lebaguette/requestmedia/views.py
@@ -13,14 +13,14 @@ from .models import Request
 def request_media(request):
     request_items = Request.objects.filter(status='N').order_by(
         '-datetime_requested')
-#    paginator = Paginator(media_items, 5)
-#    page = request.GET.get('page')
-#    try:
-#        media_items_page = paginator.page(page)
-#    except PageNotAnInteger:
-#        media_items_page = paginator.page(number=1)
-#    except EmptyPage:
-#        media_items_page = paginator.page(paginator.num_pages)
+    paginator = Paginator(request_items, 15)
+    page = request.GET.get('page')
+    try:
+        request_items_page = paginator.page(page)
+    except PageNotAnInteger:
+        request_items_page = paginator.page(number=1)
+    except EmptyPage:
+        request_items_page = paginator.page(paginator.num_pages)
     return render(request, 'requestmedia/request_media.html', locals())
 
 
