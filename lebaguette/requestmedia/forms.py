17063b2f4d74432fdd46a24c819968449cebbf30
George Goranov <g.p.goranov@gmail.com>
2016-06-20 17:06:54 +0300

    remove useless form

--- lebaguette/requestmedia/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-
-from .models import MediaItem
-
-
-class MediaItemForm(ModelForm):
-    class Meta:
-        model = MediaItem
-        fields = ['title', 'media_type', 'released', 'imdb_rating', 'imdb_id']
