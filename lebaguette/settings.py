dddd9142b563ccc794075c146fc1d28e23f1e257
George Goranov <g.p.goranov@gmail.com>
2016-12-24 11:17:00 +0200

    add dummy row

--- lebaguette/settings.py
+++ lebaguette/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = os.environ.get('DEBUG', True)
 
 # secure proxy SSL header and secure cookies
