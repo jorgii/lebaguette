41809e57a80c19878f3e1e3360a4b4954e6ecaf5
George Goranov <g.p.goranov@gmail.com>
2016-12-27 21:26:25 +0200

    handle IOErrors gently

--- requestmedia/models.py
+++ requestmedia/models.py
@@ -54,11 +54,13 @@ class MediaItem(models.Model):
                 file_name = os.path.basename(urlparse(image_url).path)
                 with open('/tmp/tmp_logo.png', 'wb') as f:
                     f.write(r.content)
-                reopen = open('/tmp/tmp_logo.png', 'rb')
-                django_file = File(reopen)
-                self.poster.save(file_name, django_file, save=True)
-                reopen.close()
-                os.remove('/tmp/tmp_logo.png')
+                    django_file = File(f)
+                    try:
+                        self.poster.save(file_name, django_file, save=True)
+                    except IOError as e:
+                        pass
+                    finally:
+                        os.remove('/tmp/tmp_logo.png')
             except requests.exceptions.MissingSchema:
                 super(MediaItem, self).save(*args, **kwargs)
         super(MediaItem, self).save(*args, **kwargs)
