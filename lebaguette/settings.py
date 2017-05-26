01eec2dc6b3b98670ce8ebd80eb635bd27c3580d
George Goranov <g.p.goranov@gmail.com>
2017-05-26 12:06:34 +0300

    Change default db host to suit docker setting

--- lebaguette/settings.py
+++ lebaguette/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
         'NAME': os.environ.get('DATABASE_NAME', 'lebaguette'),
         'USER': os.environ.get('DATABASE_USER', 'lebaguette'),
         'PASSWORD': os.environ.get('DATABASE_PASSWORD', 'lebaguette'),
-        'HOST': os.environ.get('DATABASE_HOST', 'localhost'),
+        'HOST': os.environ.get('DATABASE_HOST', 'db'),
         'PORT': os.environ.get('DATABASE_PORT', '5432'),
     }
 }
