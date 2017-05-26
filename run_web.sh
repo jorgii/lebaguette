aaf184140770d667cd0d4b083ceecdcc2ec43641
George Goranov <g.p.goranov@gmail.com>
2017-05-26 18:50:54 +0300

    Do not touch permissions

--- run_web.sh
+++ run_web.sh
@@ -5,11 +5,5 @@ echo ------------ End DB Migration ------------
 echo ------------ Begin Collect Static ------------
 python manage.py collectstatic --noinput  # Collect static files
 echo ------------ End Collect Static ------------
-
-echo ------------ Align Permissions ------------
-chown -R 1000:www-data ./  # Align permissions
-
-
-# Start uwsgi processes
 echo ------------ Start UWSGI ------------
-exec uwsgi --ini uwsgi.ini
+exec uwsgi --ini uwsgi.ini  # Start uwsgi processes
