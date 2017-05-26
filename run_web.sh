6b396fbac9507bce586403995aa77a7eb5f5dfca
George Goranov <g.p.goranov@gmail.com>
2017-05-26 19:29:27 +0300

    test

--- run_web.sh
+++ run_web.sh
@@ -7,6 +7,6 @@ python manage.py collectstatic --noinput  # Collect static files
 echo ------------ End Collect Static ------------
 echo ------------ Align Permissions ------------
 chown -R www-data:git .  # Align permissions
-# Start uwsgi processes
+# Start uwsgi processes!
 echo ------------ Start UWSGI ------------
 exec uwsgi --ini uwsgi.ini
