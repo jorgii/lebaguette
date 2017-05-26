fb5f7052cf2bc7515e3c6974c5dfe9850913590f
George Goranov <g.p.goranov@gmail.com>
2017-05-26 15:52:46 +0300

    Use new key name

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 eval "$(ssh-agent -s)" # Start the ssh agent
-echo "$PROD_KEY" > prod_key.pem
-chmod 600 prod_key.pem # This key should have push access
-ssh-add prod_key.pem
+chmod 600 .ssh/prod_key # This key should have push access
+ssh-add prod_key
 git remote add deploy $PROD_REPO_URI
 git push prod master
