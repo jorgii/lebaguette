bafeff33f6f9eba67550d116c4c5b6eab01e428d
George Goranov <g.p.goranov@gmail.com>
2017-09-05 16:38:26 +0300

    Finalise docker setup

--- run_web.dev.sh
+++ run_web.dev.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-echo ------------ Begin DB Migration ------------
+echo "------------ Wait 5 seconds for db ------------"
 sleep 5
-python manage.py migrate                  # Apply database migrations
-echo ------------ End DB Migration ------------
-echo ------------ Begin Collect Static ------------
-python manage.py collectstatic --noinput  # Collect static files
-echo ------------ End Collect Static ------------
-# Start dev web server processes!
-echo ------------ Start UWSGI ------------
-exec python manage.py runserver 0.0.0.0:8000
+echo "------------ Begin DB Migration ------------"
+python manage.py migrate
+echo "------------ End DB Migration ------------"
+echo "------------ Begin Collect Static ------------"
+python manage.py collectstatic --noinput
+echo "------------ End Collect Static ------------"
+echo "------------ Load fixtures ------------"
+python manage.py loaddata home/fixtures/users_data.json
+echo "------------ Start Web ------------"
+python manage.py runserver 0.0.0.0:8000
